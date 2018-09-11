5157b7cbb192e58ac2b2cacbad65e186579cf317
joshparkerj <superfakek@gmail.com>
2018-09-11 07:14:23 -0600

    complete second drill

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,6 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      dataList: [
+        "react",
+        "angular",
+        "vue",
+        "django",
+        "flask",
+        "rails",
+        "zope",
+        "node",
+        "express"
+      ]
+    }
+  }
+  eachItem(e,i){
+    console.log(e);
+    return (
+      <li><h2>{e}</h2></li>
+    )
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +35,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <ul>
+            {this.state.dataList.map(this.eachItem)}
+          </ul>
       </div>
     );
   }
