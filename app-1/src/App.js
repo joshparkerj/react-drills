24a8a97c4104c790e83381cdcb0237cbe1336328
joshparkerj <superfakek@gmail.com>
2018-09-10 23:38:52 -0600

    complete first drill

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -3,16 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      userInput: ''
+    };
+  }
+  updateText = input => {
+    this.setState({userInput: input.target.value})
+  }
   render() {
     return (
       <div className="App">
+        <input className="user-input" onChange={this.updateText} />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>{this.state.userInput}</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="crazy-land">
+          <div className="crazy-area num1">{this.state.userInput}</div>
+          <div className="crazy-area num2">{this.state.userInput}</div>
+          <div className="crazy-area num3">{this.state.userInput}</div>
+          <div className="crazy-area num4">{this.state.userInput}</div>
+          <div className="crazy-area num5">{this.state.userInput}</div>
+          <div className="crazy-area num6">{this.state.userInput}</div>
+          <div className="crazy-area num7">{this.state.userInput}</div>
+          <div className="crazy-area num8">{this.state.userInput}</div>
+          <div className="crazy-area num9">{this.state.userInput}</div>
+          <div className="crazy-area num10">{this.state.userInput}</div>
+          <div className="crazy-area num11">{this.state.userInput}</div>
+          <div className="crazy-area num12">{this.state.userInput}</div>
+        </div>
       </div>
     );
   }
